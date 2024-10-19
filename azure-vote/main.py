a4ede978ab655e83d696d536ba082b8880ddcdba
mrsontu <mrsontu.tn@gmail.com>
2024-10-19 15:07:59 +0700

    update main

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -10,18 +10,18 @@ from datetime import datetime
 
 # App Insights
 # TODO: Replace <INSTRUMENTATION_KEY> with your Application Insights instrumentation key
-INSTRUMENTATION_KEY = "8eb3094b-d805-4dda-8113-bf3efac2a812;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus.livediagnostics.monitor.azure.com/;ApplicationId=cb09f4d1-5ce4-4ba9-a204-195a3e903cef"
+INSTRUMENTATION_KEY = "InstrumentationKey=8eb3094b-d805-4dda-8113-bf3efac2a812;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus.livediagnostics.monitor.azure.com/;ApplicationId=cb09f4d1-5ce4-4ba9-a204-195a3e903cef"
 
 # Logging
 logger = logging.getLogger(__name__)
-logger.addHandler(AzureLogHandler(connection_string=f'InstrumentationKey={INSTRUMENTATION_KEY}'))
+logger.addHandler(AzureLogHandler(connection_string=f'{INSTRUMENTATION_KEY}'))
 logger.setLevel(logging.INFO)
 
 # Metrics
-exporter = MetricsExporter(connection_string=f'InstrumentationKey={INSTRUMENTATION_KEY}')
+exporter = MetricsExporter(connection_string=f'{INSTRUMENTATION_KEY}')
 
 # Tracing
-tracer = AzureExporter(connection_string=f'InstrumentationKey={INSTRUMENTATION_KEY}')
+tracer = AzureExporter(connection_string=f'{INSTRUMENTATION_KEY}')
 sampler = ProbabilitySampler(1.0)
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
