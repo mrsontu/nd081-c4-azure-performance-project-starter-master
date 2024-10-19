d43072e149dfcd5a15c7fa2745495e50fa813d89
mrsontu <mrsontu.tn@gmail.com>
2024-10-19 15:41:34 +0700

    run port

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -105,7 +105,6 @@ def index():
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
 if __name__ == "__main__":
-    # TODO: Use the statement below when running locally
-    app.run() 
+    app.run(host='0.0.0.0', port=5000)
     # TODO: Use the statement below before deployment to VMSS
     # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
